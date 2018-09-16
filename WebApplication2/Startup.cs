0e967fa8c72fa9aa1024a9e5cf4b3e50f8a65011
Michał Kuligowski <195000@edu.p.lodz.pl>
2018-09-16 19:04:34 +0200

    ioio

--- WebApplication2/Startup.cs
+++ WebApplication2/Startup.cs
@@ -7,7 +7,7 @@ namespace WebApplication2
     {
         public void Configuration(IAppBuilder app)
         {
-            // Any connection or hub wire up and configuration should go here
+
             app.MapSignalR();
 
         }
